c17187fcd3c8b553a5c80aa8aa1a3ccbadb03050
mahaliahr <mahalia_hr@outlook.com>
2019-12-22 23:45:57 +0000

    colour change and grid change

--- background.js
+++ background.js
@@ -3,6 +3,9 @@ let dim;
 let dimension;
 let xCompress;
 let yCompress;
+let boxWidth;
+let boxHeight;
+let changeWidth;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -18,21 +21,26 @@ function windowResized() {
 }
 
 function draw() {
+  noStroke();
+  // fill(color(252, 239, 239));
+  fill(color(161, 252, 223)); 
+  changeWidth = setInterval(() => {
+    xCompress = mouseX / 10;
+    console.log(xCompress);
+    yCompress = mouseY / 10;
+    boxWidth = dimension / 2;
+    boxHeight = dimension / yCompress;
+  },10000);
+
+
   for (let i = 0; i < width; i = i + dimension) {
     for (let j = 0; j < height; j = j + dimension) {
-      xCompress = dimension/mouseX;
-      yCompress = dimension/mouseY;
-      //console.log(xCompress);
-      noStroke();
-      fill(color(255, 100, 100));
-      rect(i, j, xCompress, yCompress);
-      //drawSquare(i, j);
+      drawSquare(i, j);
+
     }
   }
 }
 // squish square (x dimension based on mouseX, y dimension based on mouseY)
-// function drawSquare(x, y) {
-//   rect(x, y, dimension/mouseX, dimension);
-//   // console.log(xCompress);
-//   // console.log(yCompress);
-// }
+function drawSquare(x, y) {
+  rect(x, y, boxWidth, boxHeight);
+}
